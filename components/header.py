21a05c0f556a45d0de2fb196c540b392a5fc872a
Cphelps1122 <chrisphelps@ymail.com>
2026-03-15 16:11:19 -0500

    Update header.py

--- components/header.py
+++ components/header.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-def render_header(title):
-    col1, col2 = st.columns([1, 8])
-    with col1:
-        st.image("assets/cgs_logo.png", width=55)
-    with col2:
-        st.markdown(
-            f"<h1 style='margin-top: 10px;'>{title}</h1>",
-            unsafe_allow_html=True
-        )
+def render_header():
+    st.markdown(
+        """
+        <div style="display: flex; justify-content: center; margin-bottom: 20px;">
+            <img src="assets/cgs_logo2.png" style="max-width: 60%; height: auto;">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
